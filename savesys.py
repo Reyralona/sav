908f38b9debd9bc827d6cdc0f3c32b7cccdbac58
Henrique Moura <reypetrucci1@gmail.com>
2023-05-04 13:12:18 -0300

    Read from python to HTML page

---
+++ savesys.py
@@ -0,0 +1,67 @@
+import json
+
+def read(file):
+    with(open(file, "r") as data):
+        content = json.loads(data.read())
+        return content 
+def write(file, jsonobj):
+    with(open(file, "w") as data):  
+        data.write(jsonobj)
+
+def addRole(role):
+    content = read("data.json")
+    if role in content:
+        print("Registry alread exists")
+    else:
+        content[role] = []
+        jsonobj = json.dumps(content, indent=3)
+        write("data.json", jsonobj)
+
+def delRole(role):
+    content = read("data.json")
+    if role in content:
+        del content[role]
+        jsonobj = json.dumps(content, indent=3)
+        write("data.json", jsonobj)
+    else:
+        print("Registry doesn't exist")
+    
+
+def add(role, dictionary):
+    content = read("data.json")
+    students = content[role]
+    
+    if dictionary in students:
+        print("Entry was already registered.")
+    else:
+        students.append(dictionary)
+        jsonobj = json.dumps(content, indent=3)
+        write("data.json", jsonobj)
+
+def delete(role, dictionary):
+    content = read("data.json")
+    try:
+        students = content[role]
+    except KeyError:
+        print("Registry doesn't exist.")
+        return;
+
+    if dictionary in students:
+        students.pop(students.index(dictionary))
+        jsonobj = json.dumps(content, indent=3)
+        write("data.json", jsonobj)
+    else:
+        print("Entry doesn't exist")
+
+addRole("Students")
+add("Students", 
+    {"Name": "Michael Miyers", 
+    "Major" : "Physics", 
+    "RN" : 5543})
+
+addRole("Teachers")
+add("Teachers", 
+    {"Name": "Devil Maycry", 
+    "Major" : "Physics", 
+    "RN" : 5543})
+
